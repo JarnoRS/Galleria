b439e42a2a9b545a158b250e0b4ef41911b02e84
Rautakoski Jarno K <rjarno@cubbli22-gold.local>
2025-03-21 18:36:50 +0200

    Add new item form (add_image)

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/add_image")
+def add_image():
+    return render_template("add_image.html")
+
+@app.route("/create_image", methods=["POST"])
+def create_image():
+    title = request.form["title"]
+    kuvaus = request.form["description"]
+    genre = request.form["genre"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO images (title, kuvaus, genre, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [title, kuvaus, genre, user_id])
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,10 +57,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -53,5 +71,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE images (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    kuvaus TEXT,
+    genre TEXT,
+    user_id INTEGER REFERENCES users
+);
